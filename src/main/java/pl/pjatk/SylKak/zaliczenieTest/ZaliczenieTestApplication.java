a50eba46e8474e65c984e01990f587dc9ba2c914
s20163 <s20163@pjwstk.edu.pl>
2021-07-03 19:51:27 +0200

    Initial commit - setting up the project

---
+++ src/main/java/pl/pjatk/SylKak/zaliczenieTest/ZaliczenieTestApplication.java
@@ -0,0 +1,14 @@
+package pl.pjatk.SylKak.zaliczenieTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaliczenieTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaliczenieTestApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
