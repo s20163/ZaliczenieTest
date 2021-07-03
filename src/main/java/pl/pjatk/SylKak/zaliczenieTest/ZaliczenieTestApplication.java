8e51a34e1b4ea703b2855a0173eeaf76cc72e81a
s20163 <s20163@pjwstk.edu.pl>
2021-07-03 20:28:37 +0200

    Test commit

--- src/main/java/pl/pjatk/SylKak/zaliczenieTest/ZaliczenieTestApplication.java
+++ src/main/java/pl/pjatk/SylKak/zaliczenieTest/ZaliczenieTestApplication.java
@@ -8,7 +8,7 @@ public class ZaliczenieTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZaliczenieTestApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello World");
 	}
 
 }
